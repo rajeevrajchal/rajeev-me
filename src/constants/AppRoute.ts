803564efa14879a6dbd6d852c5223964da9bd4d0
Rajeev Rajchal <rajeevrajchal12@gmail.com>
2024-02-13 22:48:37 +0545

    Chore: Reset app and structures

--- src/constants/AppRoute.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export const AppRoute = {
-  HOME: '/',
-  PROJECT: '/projects',
-  BLOGS: '/blog',
-  LIBRARY: '/library',
-  SKILL: '/skill_graph',
-};
--- src/modules/home/compnents/Projects.tsx
+++
@@ -1,9 +0,0 @@
-import SectionTitle from '@components/shared/SectionTitle';
-
-const Projects = () => (
-  <div className="flex flex-col gap-4">
-    <SectionTitle index="03" label="Projects" />
-  </div>
-);
-
-export default Projects;
