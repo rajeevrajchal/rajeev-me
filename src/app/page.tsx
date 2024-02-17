872a51c4930f439f4e87efd43381aa88199551c3
Rajeev Rajchal <rajeevrajchal12@gmail.com>
2024-02-17 10:58:50 +0545

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,83 @@
-import Image from "next/image";
+"use client";
+import Link from "next/link";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { SiUpwork } from "react-icons/si";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main className="container mx-auto px-4 md:px-auto">
+      <div className="w-full h-screen grid place-content-center gap-4">
+        <p className="flex flex-col md:flex-row items-center gap-2 text-3xl">
+          Hello I am, <p className="font-bold  text-4xl">Rajeev Rajchal</p>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <p className="text-sm text-justify">
+          Hello! I'm a skilled front-end developer specializing in building
+          modern and responsive web applications. With expertise in React,
+          JavaScript, TypeScript, and HTML/CSS, I'm passionate about creating
+          user-friendly interfaces and delivering exceptional user experiences.
+        </p>
+        <ul>
+          <li>Key Skills:</li>
+          <li>
+            🎨 HTML/CSS: Experience in implementing pixel-perfect layouts and
+            styling web applications using HTML and CSS.
+          </li>
+          <li>
+            💡 JavaScript & TypeScript: Strong understanding of JavaScript and
+            TypeScript to write clean and maintainable code.
+          </li>
+          <li>
+            🌐 React: Proficient in developing interactive UI components and
+            building efficient React applications.
+          </li>
+          <li>
+            🚀 Frontend Frameworks: Familiarity with frontend frameworks such as
+            React, Next.js, and Redux.
+          </li>
+          <li>
+            📱 Responsive Design: Knowledge of responsive design principles to
+            ensure optimal user experiences across devices.
+          </li>
+          <li>
+            🔧 Testing & Debugging: Proficient in testing and debugging frontend
+            applications for optimal performance.
+          </li>
+          <li>
+            🔄 Version Control: Experience with version control systems like Git
+            to collaborate effectively with team members.
+          </li>
+        </ul>
+        <div className="flex flex-col gap-2">
+          <p>Let's Connect</p>
+          <div className="flex items-center gap-4">
+            <Link
+              href="https://www.linkedin.com/in/rajeevrajchal/"
+              target="_blank"
+            >
+              <FaLinkedin className="text-[#2566C2]" size={32} />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/rajeevrajchal/"
+              target="_blank"
+            >
+              <FaGithub size={30} />
+            </Link>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="flex flex-col gap-2">
+          <p>Get a deal ?</p>
+          <div>
+            <Link
+              href="https://www.upwork.com/freelancers/~01f0978587bed2e79c"
+              target="_blank"
+            >
+              <SiUpwork className="text-[#35A800]" size={32} />
+            </Link>
+          </div>
+        </div>
       </div>
     </main>
   );
-}
+};
+
+export default Home;
