61ea627f581e9daba79f275c359af64ff320060e
Rajeev Rajchal <rajeevrajchal12@gmail.com>
2024-04-02 22:01:31 +0545

    Feature: Added notion as a backend

---
+++ src/app/action.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { notion } from "@/libs/notion";
+import { notFound } from "next/navigation";
+
+export async function getPortfolio() {
+  try {
+    const data = await notion.databases.query({
+      database_id: process.env.NEXT_PUBLIC_NOTION_DB_PORTFOILO!,
+    });
+    return data;
+  } catch (error) {
+    notFound();
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Flex } from "@radix-ui/themes";
+import { getPortfolio } from "./action";
 
-const Home = () => {
+const Home = async () => {
+  const portfolio = await getPortfolio();
+
   return (
     <Flex direction="column" gap="2">
       <p className="text-[4rem]">I am Rajeev. I need free design suggestion</p>
+      <pre>{JSON.stringify(portfolio.results, null, 2)}</pre>
     </Flex>
   );
 };
