3ea99ed8b8eeacc4bfa6a9e7aa2a0afc5a3898c8
Rajeev Rajchal <rajeevrajchal12@gmail.com>
2025-01-25 17:15:19 +0100

    added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
 	import DebugGrid from '@comp/debug/debug-grid.svelte';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
+
 	import '@style/global.scss';
+
 	const { children } = $props();
 </script>
 
 {@render children()}
-<DebugGrid />
 
-<style lang="scss">
-	.main {
-		position: relative;
-		width: 100svw;
-		min-height: 100svh;
-	}
-</style>
+{#if dev}
+	<DebugGrid />
+{/if}
