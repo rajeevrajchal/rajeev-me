82196ed21f94506b08529e5aa774985829155ac7
Rajeev Rajchal <rajeevrajchal12@gmail.com>
2025-04-07 23:49:23 +0200

    update the basic setting

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '@styles/reset.css';
+	import '@styles/global.css';
+	import type { Snippet } from 'svelte';
+
+	let {
+		children
+	}: {
+		children: Snippet;
+	} = $props();
+</script>
+
+{@render children()}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,37 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import browserslist from 'browserslist';
+import { browserslistToTargets, composeVisitors, Features } from 'lightningcss';
+
+import { colorFunctionsVisitor } from 'lightningcss-plugin-color-functions';
+import { inlineSvgVisitor } from 'lightningcss-plugin-inline-svg';
+import { envWhitespaceFix } from 'lightningcss-plugin-env-whitespace-fix';
+
 import { defineConfig } from 'vite';
 
+import { resolve } from 'node:path';
+
 export default defineConfig({
+	css: {
+		transformer: 'lightningcss',
+		lightningcss: {
+			targets: browserslistToTargets(browserslist('>= 0.25%')),
+			include: Features.Nesting,
+			exclude: Features.LogicalProperties | Features.DirSelector | Features.LightDark,
+			drafts: {
+				customMedia: true
+			},
+			visitor: composeVisitors([
+				colorFunctionsVisitor,
+				envWhitespaceFix,
+				inlineSvgVisitor({
+					directory: resolve(__dirname, './src/lib/shared/icons'),
+					acceptedExtensions: ['.svelte']
+				})
+			])
+		}
+	},
+	build: {
+		cssMinify: 'lightningcss'
+	},
 	plugins: [sveltekit()]
 });
