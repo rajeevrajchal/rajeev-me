d10113c3501376e25d44e169440a793357422863
Rajeev Rajchal <rajeevrajchal12@gmail.com>
2025-07-29 00:13:46 +0200

    update the hero animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,35 @@
 	import { SplitText } from '@shared/components';
 	import { Github, Linkedin } from 'lucide-svelte';
 	import { github, linkedin } from '@shared/constant/rajeev.json';
+	import { animate, stagger } from 'animejs';
+
+	let elements: HTMLElement[] = $state([]);
+
+	$effect(() => {
+		if (!elements) return;
+
+		animate(elements, {
+			opacity: {
+				from: 0,
+				to: 1
+			},
+			y: {
+				from: 50,
+				to: 0
+			},
+			duration: 800,
+			delay: stagger(50, { start: 100 }),
+			easing: 'easeOutExpo'
+		});
+	});
 </script>
 
 <section>
-	<img class="profile" src="./images/rajeev.png" alt="Rajeev Rajchal" />
+	<img bind:this={elements[1]} class="profile" src="./images/rajeev.png" alt="Rajeev Rajchal" />
 	<SplitText value="Rajeev Rajchal" size="large" direction="y" />
 	<SplitText value="Software Developer" direction="y" delay={1000} />
-	<div class="socials">
-		<a class="icon" href={linkedin} target="_blank"><Linkedin /></a>
+	<div class="socials" bind:this={elements[2]}>
+		<a class="icon" href={linkedin} target="_blanwk"><Linkedin /></a>
 		<a class="icon" href={github} target="_blank"><Github /></a>
 	</div>
 </section>
@@ -41,6 +62,7 @@
 		align-self: center;
 		box-shadow: 0 8px 32px rgba(47, 69, 80, 0.2);
 		border: 4px solid #b8dbd9;
+		opacity: 0;
 	}
 
 	.socials {
@@ -48,6 +70,7 @@
 		align-items: center;
 		justify-content: center;
 		gap: calc(env(--gap-2) * 2);
+		opacity: 0;
 	}
 
 	.icon {
